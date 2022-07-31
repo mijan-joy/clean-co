d6cb8718c8fbc1ef1c1b5787fcbed7008ef4960e
mijan-joy <mmrsm46@gmail.com>
2022-08-01 00:45:08 +0600

    component add

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#176F6B",
+          secondary: "#FFC000",
+          accent: "#FFEDD5",
+          neutral: "#f3f4f6",
+          "base-100": "#FFFFFF",
+          info: "#98A8DD",
+          success: "#1BBB70",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
